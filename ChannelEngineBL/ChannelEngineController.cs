ad12701eaff63c0ac82feda3cd83b74290bc8033
Peter Huppertz <peter@dnl-core.net>
2020-09-22 20:09:16 +0200

    Created separate project for business logic

---
+++ ChannelEngineBL/ChannelEngineController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChannelEngine;
+
+namespace ChannelEngineBL
+{
+    public class ChannelEngineController
+    {
+
+        IRepository repository = null;
+
+        public ChannelEngineController() 
+        {
+            repository = new Repository();
+        }
+
+        public ChannelEngineController(IRepository repository)
+        {
+            if (repository != null)
+            {
+                this.repository = repository;
+            }
+            else
+            {
+                this.repository = new Repository();
+            }
+        }
+
+        public async Task<IList<Order>> GetOutstandingOrders() 
+        {
+            var orders = await repository.GetOrdersInProgress();
+            return orders;
+        }
+
+        public IList<OrderLine>TakeTopNProductsFromOrders(IList<Order> orders, int take)
+        {
+            IList<OrderLine> result = null;
+            List<OrderLine> lines = new List<OrderLine>();
+            foreach (var order in orders)
+            {
+                lines.AddRange(order.Lines);    
+            }
+            result = lines.OrderByDescending(l => l.Quantity).Take(take).ToList();
+
+            return result;            
+        }
+    }
+}
